3701963d09973d6675d326053fb21774ee36590f
4andrew2 <andrey.tixomirovv@gmail.com>
2022-03-31 11:23:26 +0300

    Добавлена тестовая система распознавания и изображение - пример

---
+++ main.py
@@ -0,0 +1,47 @@
+import cv2
+import numpy as np
+
+# Read the original image
+img = cv2.imread('test.jpg')
+h = img.shape[0]
+w = img.shape[1]
+print(h, w)
+img = cv2.resize(img, (int(h*0.7), int(w*0.7)))
+# Display original image
+cv2.imshow('Original', img)
+cv2.waitKey(0)
+
+# Convert to graycsale
+img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# Blur the image for better edge detection
+img_blur = cv2.GaussianBlur(img_gray, (3, 3), 0)
+
+# Sobel Edge Detection
+sobelx = cv2.Sobel(src=img_blur, ddepth=cv2.CV_64F, dx=1, dy=0, ksize=5) # Sobel Edge Detection on the X axis
+sobely = cv2.Sobel(src=img_blur, ddepth=cv2.CV_64F, dx=0, dy=1, ksize=5) # Sobel Edge Detection on the Y axis
+sobelxy = cv2.Sobel(src=img_blur, ddepth=cv2.CV_64F, dx=1, dy=1, ksize=5) # Combined X and Y Sobel Edge Detection
+# Display Sobel Edge Detection Images
+cv2.imshow('Sobel X', sobelx)
+cv2.waitKey(0)
+cv2.imshow('Sobel Y', sobely)
+cv2.waitKey(0)
+cv2.imshow('Sobel X Y using Sobel() function', sobelxy)
+cv2.waitKey(0)
+
+# Canny Edge Detection
+edges = cv2.Canny(image=img_blur, threshold1=100, threshold2=200) # Canny Edge Detection
+# Display Canny Edge Detection Image
+cv2.imshow('Canny Edge Detection', edges)
+cv2.waitKey(0)
+
+contours, hierarchy = cv2.findContours(edges, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+# Find the index of the largest contour
+areas = [cv2.contourArea(c) for c in contours]
+max_index = np.argmax(areas)
+cnt = contours[max_index]
+cv2.drawContours(img, contours, max_index, (0, 255, 0), 3)
+cv2.imshow('result', img)
+cv2.waitKey(0)
+
+cv2.destroyAllWindows()
